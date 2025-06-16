f1a6c81d056336ce1577dee883f843297fc8a632
Saurav Kumar <148621846+kumarsaurav007@users.noreply.github.com>
2025-06-17 01:25:20 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,18 +1,10 @@
-# from django.core.management import execute_from_command_line
-# import os
-# import sys
-
-# if __name__ == '__main__':
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_project.settings')
-#     execute_from_command_line(sys.argv)
-
-#!/usr/bin/env python
+from django.core.management import execute_from_command_line
 import os
 import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_project.settings')
-    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
+
+
 
