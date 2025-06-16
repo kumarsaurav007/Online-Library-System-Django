ca37272c25902207e1953a827ff007c7a9f8cac9
Saurav Kumar <148621846+kumarsaurav007@users.noreply.github.com>
2025-06-17 01:21:42 +0530

    Update models.py

--- library/models.py
+++ library/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    email = models.EmailField
     bio = models.TextField()
 
     def __str__(self):
